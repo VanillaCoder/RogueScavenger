8587c9a34bec93f646829f5716128d829a9d0878
VanillaCoder <liamhearts@gmail.com>
2019-12-19 02:45:19 -0800

    instant double jump fixed, double jump implemented

--- src/index.js
+++ src/index.js
@@ -11,34 +11,64 @@ let player = new Player(GAME_WIDTH, GAME_HEIGHT)
 
 //sets an initial lastTime
 let lastTime = 0;
+//**********************************************
+//section adds event listeners for keypresses, function is then called in gameLoop()
+var keyState = {
+    37: false,
+    38: false,
+    39: false,
+    87: false,
+    65: false,
+    68: false
+};
 
-//adds event listeners for keypresses, function is then called in gameLoop()
-var keyState = {};
+let keyDownEvent = false;
+let keyUpEvent = false;
+
 
 window.addEventListener('keydown', function (e) {
+    if(e.keyCode === (87 || 38)){
+        keyDownEvent = true;
+    }
     keyState[e.keyCode || e.which] = true;
 }, true);
 
 window.addEventListener('keyup', function (e) {
+    if(e.keyCode === (87 || 38)) {
+        keyUpEvent = false;
+    }
     keyState[e.keyCode || e.which] = false;
 }, true);
 
+
+
 function inputHandler() {
-    if (keyState[38] || keyState[87]){
-        player.jump(player.gravity);
+    if (keyState[38] || keyState[87] && keyDownEvent && !keyUpEvent) {
+        keyUpEvent = true;
+        player.jump();
     }
-    if (keyState[37] || keyState[65]){
+    if (keyState[37] || keyState[65]) {
         player.moveLeft();
-    }  
-    else if (keyState[39] || keyState[68]){
+    }
+    else if (keyState[39] || keyState[68]) {
         player.moveRight();
     }
     else {
         player.speed = 0;
-    }  
+    }
 }
-
+//weird proxy shit trying to get double juump to work prob outdated now
+// var targetProxy = new Proxy(keyState, {
+//   set: function (target, key, value) {
+//       if(key === "87" && value === true){
+//           player.jump();
+//       }
+//       target[key] = value;
+//       return true;
+//   }
+// });
 // ************************************************
+
 
 function gameLoop(timestamp) {
     let deltaTime = timestamp - lastTime;
--- src/player.js
+++ src/player.js
@@ -7,7 +7,7 @@ export default class Player {
         this.maxSpeed = 3;
         this.speed = 0;
         this.jumpVel = 0;
-        this.jumpAvailable = true;
+        this.jumpAvailable = 2;
         this.position = {
             x: gameWidth / 2 - this.width / 2,
             y: gameHeight - this.height - 10,
@@ -28,29 +28,35 @@ export default class Player {
         ctx.fillStyle = "#f00"
         ctx.fillRect(this.position.x, this.position.y, this.width, this.height)
     }
-    
+
     jump() {
-        if(this.jumpAvailable === true) {
-            this.jumpAvailable = false;
+        if (this.jumpAvailable === 2) {
+            this.jumpAvailable -= 1;
+            this.jumpVel += 7;
+        }
+        else if (this.jumpAvailable === 1) {
+            this.jumpAvailable -= 1;
+            this.jumpVel /= 3;
             this.jumpVel += 5;
         }
     }
-   
+
 
     update(deltaTime) {
         if (!deltaTime) return;
         this.position.x += this.speed;
-        if(this.jumpVel > 0 || this.jumpAvailable === false) {
+        if (this.jumpVel > 0 || this.jumpAvailable < 2) {
             this.position.y -= this.jumpVel;
-            this.jumpVel -= .1;
+            this.jumpVel -= .18;
         }
-        if(this.position.x < 0) this.position.x = 0;
-        if(this.position.x > 800 - this.width) this.position.x = 800 - this.width;
-        if(this.position.y < 0) this.position.y = 0;
-        if(this.position.y > 600 - this.height) {
+        if (this.position.x < 0) this.position.x = 0;
+        if (this.position.x > 800 - this.width) this.position.x = 800 - this.width;
+        if (this.position.y < 0) this.position.y = 0;
+        if (this.position.y > 600 - this.height) {
             this.position.y = 600 - this.height;
             this.jumpVel = 0;
-            this.jumpAvailable = true;
+            this.jumpAvailable = 2;
+            console.log("Bottom")
         };
 
 
