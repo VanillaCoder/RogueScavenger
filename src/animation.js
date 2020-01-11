934b8acbfbf01b6f45d4620d1148056ce5c73418
VanillaCoder <liamhearts@gmail.com>
2020-01-11 14:06:15 -0800

    working left/right/idle animation, wow finally

--- src/animation.js
+++ src/animation.js
@@ -9,54 +9,83 @@ export default class Animator {
         this.delay = 25;
         this.frame = 0;
         this.frame_index = 0;
-        this.frame_set = 
+        this.frame_set =
             [
-                [0,1,2,3],
-                [9, 10, 11, 12, 13, 14]
+                [0, 1, 2, 3],
+                [9, 10, 11, 12, 13, 14],
+                [32, 33, 34, 35, 36, 37]
             ]
-        
+
 
     }
 
     update() {
-        // if(this.player.speed > 0) {
-        //     this.direction = 1;
-        // }
-        // if(this.player.speed < 0) {
-        //     this.direction = 1;
-        // }
-        // if(this.player.speed === 0) {
-        //     this.direction = 0;
-        // }     
-        this.count ++;
+        this.count++;
         if (this.count >= this.delay) {
             this.count = 0;
             this.frame_index = (this.frame_index == this.frame_set[this.direction].length - 1) ? 0 : this.frame_index + 1;
             this.frame = this.frame_set[this.direction][this.frame_index]
+            // console.log(this.frame, this.frame_index)            
+
         }
     }
 
-    draw(ctx) {   
-        
-        let currentFrame = this.frame_set[this.direction][this.frame_index] + 1;
+    changeDirection(direction) {
+        this.frame_index = 0;
+        this.direction = direction;
+    }
+
+    draw(ctx) {
+
+        if (this.frame_set[this.direction][this.frame_index] >= 0) {
+            var currentFrame = this.frame_set[this.direction][this.frame_index] + 1
+        }
         if (currentFrame * 48.125 > 385) {
             while (currentFrame * 48.125 > 385) {
                 this.spriteCount += 1;
                 currentFrame -= 8;
             }
         }
-        // console.log(this.spriteCount, currentFrame)
-        ctx.drawImage(
-            this.image,
-            ((currentFrame - 1) * 50),
-            (this.spriteCount * 37),
-            48,
-            37,
-            this.player.position.x - 20,
-            this.player.position.y - 10,
-            72,
-            55.5
-        );
+        console.log(this.frame_index)
+        if (this.direction === 2) {
+            ctx.drawImage(
+                this.image,
+                ((currentFrame - 1) * 50),
+                ((this.spriteCount * 37) + 10),
+                48,
+                37,
+                this.player.position.x - 42,
+                this.player.position.y - 2,
+                72,
+                55.5
+            );
+        }
+        else if (this.direction === 1) {
+            ctx.drawImage(
+                this.image,
+                ((currentFrame - 1) * 50),
+                ((this.spriteCount * 37) + 10),
+                48,
+                37,
+                this.player.position.x - 40,
+                this.player.position.y - 5,
+                72,
+                55.5
+            );
+        }
+        else {
+            ctx.drawImage(
+                this.image,
+                ((currentFrame - 1) * 50),
+                (this.spriteCount + 10),
+                48,
+                37,
+                this.player.position.x - 35,
+                this.player.position.y - 4,
+                72,
+                55.5
+            );
+        }
         this.spriteCount = 0;
     }
 
--- src/inputHandler.js
+++ src/inputHandler.js
@@ -6,6 +6,8 @@ export default class InputHandler {
         this.animator = animator;
         this.keyDownEvent = false;
         this.keyUpEvent = false;
+        this.animationChange = false;
+        this.animationChangeChk = false;
 
         this.keyState = {
             37: false,
@@ -19,16 +21,22 @@ export default class InputHandler {
         window.addEventListener('keydown', (e) => {
             if (e.keyCode === 87 || e.keyCode === 38) {
                 this.keyDownEvent = true;
-                this.animator.count = 0;
-
             }
+            console.log(e.keyCode)
+            if (e.keyCode === 39 || e.keyCode === 68 || e.keyCode === 37 || e.keyCode === 65) {
+                this.animationChange = true;
+            }
+            console.log(this.animationChange)
             this.keyState[e.keyCode || e.which] = true;
         }, true);
 
         window.addEventListener('keyup', (e) => {
             if (e.keyCode === 87 || e.keyCode === 38) {
                 this.keyUpEvent = false;
-                this.animator.count = 0;
+            }
+            if (e.keyCode === 39 || e.keyCode === 68 || e.keyCode === 37 || e.keyCode === 65) {
+                this.animationChangeChk = false;
+                this.animator.changeDirection(0)
             }
             this.keyState[e.keyCode || e.which] = false;
         }, true);
@@ -39,21 +47,25 @@ export default class InputHandler {
     keyHandler() {
         if ((this.keyState[38] || this.keyState[87]) && (this.keyDownEvent && !this.keyUpEvent)) {
             this.keyUpEvent = true;
-            console.log(this.keyUpEvent)
 
             this.player.jump();
         }
         if (this.keyState[37] || this.keyState[65]) {
             this.player.moveLeft();
+            if(this.animationChange && !this.animationChangeChk){
+                this.animationChangeChk = true;
+                this.animator.changeDirection(2);
+            }
         }
         else if (this.keyState[39] || this.keyState[68]) {
             this.player.moveRight();
-            this.animator.direction = 1;
-
+            if(this.animationChange && !this.animationChangeChk){
+                this.animationChangeChk = true;
+                this.animator.changeDirection(1);
+            }
         }
         else {
             this.player.speed = 0;
-            this.animator.direction = 0;
         }
     }
 }
